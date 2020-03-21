12b2b02f0f2a2aa4ea8d298668d553f5e832b578
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2020-03-21 09:22:50 -0300

    Criação da inserção de dados e adição do gitCard

--- gitcard.js
+++ gitcard.js
@@ -1,19 +1,44 @@
-const test = (data) => {
-    console.log('teste');
+// Select elements
+const inputText = document.querySelector('#usuario-github');
+const showButton = document.querySelector('#buscar-github');
+const gitCardContainer = document.querySelector('.github-card');
+
+const addGitCard = (content) => {
+    gitCardContainer.innerHTML = content;
 }
 
-const getGitHubInfo = (username) => {
+const insertData = (data) => {
+    const simpleUrl = data.html_url.replace('https://', '');
+
+    const gitCardContent = `
+        <div class="header User"></div>
+        <a class="avatar" href="${data.html_url}" target="_top"><img src="${data.avatar_url}" alt="${data.name}"></a>
+        <div class="content">
+            <h1>${data.name}</h1>
+            <ul class="status">
+                <li><a href="${data.html_url}?tab=repositories" target="_top"><strong>${data.public_repos}</strong>Repositórios</a></li>
+                <li><a href="https://gist.${simpleUrl}" target="_top"><strong>${data.public_gists}</strong>Gists</a></li>
+                <li><a href="${data.html_url}/followers" target="_top"><strong>${data.followers}</strong>Seguidores</a></li>
+            </ul>
+        </div>
+    `;
+
+    addGitCard(gitCardContent);
+}
+
+const getGitHubInfo = () => {
+    const username = inputText.value;
+
     console.log('executou');
     var url = 'https://api.github.com/users/' + username;
 
     var ajax = new XMLHttpRequest();
 
     ajax.onreadystatechange = function () {
-        if(this.readyState == 4 && this.status == 200){
+        if (this.readyState == 4 && this.status == 200){
             let response = JSON.parse(this.responseText);
 
-            console.log(response);
-            test(response);
+            insertData(response);
         }
     };
 
@@ -21,4 +46,4 @@ const getGitHubInfo = (username) => {
     ajax.send();
 };
 
-getGitHubInfo('johnnyferreiradev');
+showButton.addEventListener('click', getGitHubInfo);
